62170bb4c8c84b51b89736ec80a3ae08753b21fe
saya3838-hub <ssyaakiemi3838@gmail.com>
2025-11-15 19:14:48 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const map = new maplibregl.Map({
   container: "map",
   center: [140.077744, 37.598456], // 中心座標
-  zoom: 8, // ズームレベル
+  zoom: 12, // ズームレベル
   style: {
     // スタイル仕様のバージョン番号。8を指定する
     version: 8,
